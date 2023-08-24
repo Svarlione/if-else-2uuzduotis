f3026c3db7161e47f31152c48c49c4ddf2cc4995
Svarlione <ruslan.brigockij@gmail.com>
2023-08-24 18:30:50 +0300

    Add project files.

---
+++ if else 2#uzduotis/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Sveiki, iveskite prasom skaiciu, aciu.");
+string userInput = Console.ReadLine();
+int userText = Convert.ToInt32(userInput);
+if (userText % 5 == 0)
+    Console.WriteLine("Skaicius dalijasi is 5");
+else if (userText % 2 == 0)
+    Console.WriteLine("Skaicius yra lyginis");
